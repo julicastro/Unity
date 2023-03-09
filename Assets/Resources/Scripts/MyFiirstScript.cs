fd9204a2114c6784c1e9e5e0204ab3b307e795e1
julicastro <castroejulian@gmail.com>
2023-03-09 04:12:13 -0300

    canvas, ui objects, camera, C# variables

--- Assets/Resources/Scripts/MyFiirstScript.cs
+++ Assets/Resources/Scripts/MyFiirstScript.cs
@@ -1,19 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
-public class MyFiirstScript : MonoBehaviour
-{
-    // Start is called before the first frame update
+/*
+ * Integer - for counting, indexing, id
+ * float - for forces, speed, acceleration, time
+ * booleans - true or false
+ * string - store information
+ * Vector2 - to represent XY position, anything 2D related
+ * Vector3 - in order to make use of formulas
+ * GameObjects - Prefabs, anything that carries a component
+ * 
+ */
+public class MyFiirstScript : MonoBehaviour {
 
-    void Start()
-    {
-        print("Hello World");
+    // public to see on unity screen
+    /*
+    public int myInt = 1; 
+    public float myFloat = 0.0f;
+    public string mySting = "Hola";
+    public bool myBoolean = true;
+
+    public Vector2 myDesiredCord2;
+    public Vector3 myDesiredCord3;
+    public GameObject myGameObject;
+    */
+
+    public float myTime = 0;
+    public float myTestFloat;
+    public float mySpeed;
+
+    public int myIntMin, myIntMax;
+
+    void Start() {
+        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        print("Hello World");
+
+    void Update() {
+        // myTime = myTime + Time.deltaTime;
+        // print(Random.Range(1f, 10f)); // inclusive & exclusive
+        // myTestFloat = Mathf.MoveTowards(myTestFloat, myIntMax, mySpeed * Time.deltaTime); // contador: arranca del min, va a la velocidad de mySpeed y llega hasta el max
+        // si pongo speed 2 va a ir al doble
+        myTestFloat = Mathf.MoveTowards(myTestFloat, myIntMax, mySpeed * Time.deltaTime); // contador: arranca del min, va a la velocidad de mySpeed y llega hasta el max
+
+
     }
+
 }
