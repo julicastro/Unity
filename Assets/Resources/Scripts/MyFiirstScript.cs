04f285c683eadd7a015609c623c09f3fc704148b
julicastro <castroejulian@gmail.com>
2023-03-13 02:46:53 -0300

    C# for loop

--- Assets/Resources/Scripts/MyFiirstScript.cs
+++ Assets/Resources/Scripts/MyFiirstScript.cs
@@ -16,35 +16,35 @@ using UnityEngine;
  */
 public class MyFiirstScript : MonoBehaviour {
 
-    // public to see on unity screen
-    /*
     public int myInt = 1; 
-    public float myFloat = 0.0f;
+    public float myFirstFloat = 0.0f;
     public string mySting = "Hola";
     public bool myBoolean = true;
 
     public Vector2 myDesiredCord2;
     public Vector3 myDesiredCord3;
     public GameObject myGameObject;
-    */
-
-    /*
+    
     public float myTime = 0;
     public float myTestFloat;
     public float mySpeed;
 
     public int myIntMin, myIntMax;
-    */
 
     public int intMin, intMax;
     public float myFloat;
     public float speed;
     public bool limitReached = true;
     public GameObject myQuad;
+    public GameObject phineas;
     public Vector2 position;
+
+    public GameObject myParentObject;
 
     void Start() {
         myQuad.transform.position = new Vector2(intMin, myQuad.transform.position.y);
+        phineas.transform.position = new Vector2(intMin, phineas.transform.position.y);
+        
     }
 
 
@@ -53,10 +53,10 @@ public class MyFiirstScript : MonoBehaviour {
         // print(Random.Range(1f, 10f)); // inclusive & exclusive
         // myTestFloat = Mathf.MoveTowards(myTestFloat, myIntMax, mySpeed * Time.deltaTime); // contador: arranca del min, va a la velocidad de mySpeed y llega hasta el max
         // si pongo speed 2 va a ir al doble
-        myFunction();
-
-
-
+        // myFunction();
+        // myFunctionPhineas();
+        // print(myParentObject.transform.childCount);
+        myForLoop1();
     }
 
     public void myFunction() {
@@ -76,5 +76,38 @@ public class MyFiirstScript : MonoBehaviour {
         position = new Vector2(myFloat, myQuad.transform.position.y);
         myQuad.transform.position = position;
     }
+    public void myFunctionPhineas() {
+        if (limitReached) {
+            myFloat = Mathf.MoveTowards(myFloat, intMax, speed * Time.deltaTime);
+
+            if (myFloat >= intMax-0.001f) {
+                limitReached = false;
+            }
+        } else if (!limitReached) {
+            myFloat = Mathf.MoveTowards(myFloat, intMin, intMax * Time.deltaTime);
+
+            if (myFloat <= intMin+0.001f) {
+                limitReached = true;
+            }
+        }
+        position = new Vector2(myFloat, phineas.transform.position.y);
+        phineas.transform.position = position;
+    }
+    public void myForLoop1() {
+        for (int i = 0; i < myParentObject.transform.childCount; i++) {
+            int id = myParentObject.transform.GetChild(i).transform.GetComponent<ObjectInfo>().myID;
+            myParentObject.transform.GetChild(i).transform.GetComponent<ObjectInfo>().myID = i * 3;
+            myParentObject.transform.GetChild(i).transform.GetComponent<ObjectInfo>().myName = ("Child + " + id).ToString();
+            myParentObject.transform.GetChild(i).transform.GetComponent<ObjectInfo>().attack = i + 100;
+
+
+        }
+    }
+
+
+
+
+
+
 
 }
---
+++ Assets/Resources/Scripts/ObjectInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInfo : MonoBehaviour
+{
+    public string myName;
+    public int myID;
+    public float attack;
+}
